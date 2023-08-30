7e845a91e7f097b0557b2faab50c767479fa86bd
Rafael Domingues <rafaelcodomingues@gmail.com>
2023-08-30 14:17:24 -0300

    Adjust

--- music-player/src/main.jsx
+++ music-player/src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx';
 
 import './index.css';
 
-// eslint-disable-next-line no-undef
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
